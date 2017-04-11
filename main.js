d712178a09b281577adbe26c4ba2d2ad0f93a474
Artemiy Boykov <boykov@easy-pawn.ru>
2017-04-11 22:00:30 +0300

    *

--- main.js
+++ main.js
@@ -1,14 +1,25 @@
-var request = require('request');
-var jsdom = require('jsdom');
-var jquery = require('jquery');
-var beautify = require("json-beautify");
-var randomUserAgent = require('bluebird').promisifyAll(require('random-http-useragent'));
-var express = require('express');
-var app = express();
-
-//Точка в стоимости товара
+var request = require('request'),
+	jsdom = require('jsdom'),
+	jquery = require('jquery'),
+	beautify = require("json-beautify"),
+	randomUserAgent = require('bluebird').promisifyAll(require('random-http-useragent')),
+	path = require('path'),
+	fs = require('fs'),
+	argv = require('yargs').argv;
 
 var global_list = {};
+
+function download(uri, filename) {
+	return new Promise((resolve, reject) => {
+		request.head(uri, function(err, res, body){
+			console.log('content-type:', res.headers['content-type']);
+			console.log('content-length:', res.headers['content-length']);
+			request(uri).pipe(fs.createWriteStream(filename)).on('close', function() {
+				resolve();
+			}).on('error', reject);
+		});
+	});
+}
 
 function get_single_page(group, page) {
 	return new Promise(async (resolve, reject) => {
@@ -64,66 +75,67 @@ function get_single_page(group, page) {
 	});
 }
 
-/*app.get("/getupto/:group/:page", async (req, res) => {
-	var lists = [];
-	var list;
-	var upto = req.params.page-0;
-	var group = req.params.group;
-	console.log("begin");
-	for(var i = 0; i < upto; i++) {
-		console.log(group + "-" + i);
-		list = await get_single_page(group, i);
-		lists.push(list);
-	}
-	res.send(beautify(lists, null, 8, 100).replace(/\n/g, "<br/>").replace(/\s/g, "&nbsp;"));
-});*/
-
-app.get("/get/:group/:page", async (req, res) => {
-	var list = await get_single_page(req.params.group, req.params.page);
-	global_list = list;
-	res.send(beautify(list, null, 8, 100).replace(/\n/g, "<br/>").replace(/\s/g, "&nbsp;"));
-});	
-
-app.get("/example", (req, res) => {
-	 res.redirect('/get/30001062/2');
-});
-
-
-
-
 var mysql = require('promise-mysql');
 var connection;
  
 pool = mysql.createPool({
 	host: 'localhost',
-	user: 'root',
-	password: '',
-	database: 'opencart',
+	user: 'coun', //coun
+	password: 'yyyzzz002V', //yyyzzz002V
+	database: 'coun.shop', //coun.shop
 	connectionLimit: 10
 });
 
-function update_item(item, res) {
-	var title = item.title;
-	var uc_code = item.id;
-	var price = item.price;
-	var image = item.image;
-	
-	var sql = "INSERT INTO `oc_product` (`product_id`, `model`, `sku`, `upc`, `ean`, `jan`, `isbn`, `mpn`, `location`, `quantity`, `stock_status_id`, `image`, `manufacturer_id`, `shipping`, `price`, `points`, `tax_class_id`, `date_available`, `weight`, `weight_class_id`, `length`, `width`, `height`, `length_class_id`, `subtract`, `minimum`, `sort_order`, `status`, `viewed`, `date_added`, `date_modified`) VALUES (NULL, '" + title + "', '', '" + uc_code + "', '', '', '', '', '', '939', '7', 'catalog/demo/htc_touch_hd_1.jpg', '5', '1', '" + price + "', '200', '9', '2009-02-03', '146.40000000', '2', '0.00000000', '0.00000000', '0.00000000', '1', '1', '1', '0', '1', '0', '2009-02-03 16:06:50', '2017-04-09 19:50:22')";
-	
-	pool.query(sql).then(function(item){
-		var product_id = item.insertId;
-		var sql2 = "INSERT INTO `oc_product_description` (`product_id`, `language_id`, `name`, `description`, `tag`, `meta_title`, `meta_description`, `meta_keyword`) VALUES ('" + product_id + "', '1', '" + title + "', '124124', '', '124124', '', '');"
-		pool.query(sql2).then(function(item){
-			res.json("Okay!");
+function compute_description_from_props(props) {
+	return "<ul>"+props.map((a)=>`<li>${a}</li>`).reduce((a,b)=>a+b)+"</ul>";
+}
+
+function update_item(item, category_id) {
+	return new Promise(async (resolve, reject) => {
+		console.log("step 1");
+		
+		var title = item.title;
+		var product_id = item.id;
+		var price = item.price;
+		var description = compute_description_from_props(item.props);
+		var image = "catalog/coun/" + path.basename(item.image);
+		
+		if (!fs.existsSync("../public_html/image/" + image)) {
+			await download(item.image, "../public_html/image/" + image);
+		}
+		
+		console.log("step 2");
+		
+		var sql = `INSERT INTO oc_product (product_id, model, sku, upc, ean, jan, isbn, mpn, location, quantity, stock_status_id, image, manufacturer_id, shipping, price, points, tax_class_id, date_available, weight, weight_class_id, length, width, height, length_class_id, subtract, minimum, sort_order, status, viewed, date_added, date_modified) VALUES (${product_id}, '${title}', '', '', '', '', '', '', '', '6', '7', '${image}', 0, '1', '${price}', '0', '9', '2009-02-03', '146.40000000', '2', '0.00000000', '0.00000000', '0.00000000', '1', '1', '1', '0', '1', '0', '2009-02-03 16:06:50', '2017-04-09 19:50:22') ON DUPLICATE KEY UPDATE model=VALUES(model), sku=VALUES(sku), upc=VALUES(upc), ean=VALUES(ean), jan=VALUES(jan), isbn=VALUES(isbn), mpn=VALUES(mpn), location=VALUES(location), quantity=VALUES(quantity), stock_status_id=VALUES(stock_status_id), image=VALUES(image), manufacturer_id=VALUES(manufacturer_id), shipping=VALUES(shipping), price=VALUES(price), points=VALUES(points), tax_class_id=VALUES(tax_class_id), date_available=VALUES(date_available), weight=VALUES(weight), weight_class_id=VALUES(weight_class_id), length=VALUES(length), width=VALUES(width), height=VALUES(height), length_class_id=VALUES(length_class_id), subtract=VALUES(subtract), minimum=VALUES(minimum), sort_order=VALUES(sort_order), status=VALUES(status), viewed=VALUES(viewed), date_added=VALUES(date_added), date_modified=VALUES(date_modified);`;
+		
+		console.log("step 3");
+		
+		pool.query(sql).then(function(item){
+			var sql2 = `REPLACE INTO oc_product_description (product_id, language_id, name, description, tag, meta_title, meta_description, meta_keyword) VALUES ('${product_id}', '1', '${title}', '${description}', '', '${title}', '', '');`;
+			console.log("step 4");
+			pool.query(sql2).then(function(item){
+				var sql3 = `REPLACE INTO oc_product_to_category (product_id, category_id) VALUES (${product_id}, ${category_id});`;
+				console.log("step 5");
+				pool.query(sql3).then(function(item){
+					var sql4 = `REPLACE INTO oc_product_to_store (product_id, store_id) VALUES (${product_id}, 0);`;
+					pool.query(sql4).then(function(item){
+						resolve();
+					})
+				});
+			});
 		});
 	});
 }
 
-app.get("/dor", (req, res) => {
-	var f = global_list[0];
-	update_item(f, res);
-});
+async function start() {
+	var group = argv.group || 30001062;
+	var page = argv.page || 2;
+	var list = await get_single_page(group, page);
+	for(var i = 0; i < list.length; i++) {
+		await update_item(list[i], group);
+	}
+	console.log("END.");
+	process.exit();
+}
 
-app.listen(99, function () { //3030
-	console.log('coun stated');
-});
+start();
